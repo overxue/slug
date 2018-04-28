6e652263d63d387518d2edc210b73843f0d186d1
XueCong <xuecong98@gmail.com>
2018-04-28 06:25:28 +0000

    Params edit null

--- src/Slug.php
+++ src/Slug.php
@@ -11,7 +11,7 @@ class Slug
 
     protected $config;
 
-    public function __construct(array $config = [])
+    public function __construct($config = [])
     {
         $this->config = $config;
     }
@@ -26,7 +26,13 @@ class Slug
         if ($this->isEnglish($text)) {
             return str_slug($text);
         }
-        $url = $this->getTranslateUrl($text);
+        $appid = $this->config['appId'];
+        $key = $this->config['appKey'];
+        // 如果没有配置百度翻译，自动使用兼容的拼音方案
+        if (empty($appid) || empty($key)) {
+            return $this->pinyin($text);
+        }
+        $url = $this->getTranslateUrl($appid, $key, $text);
         // 实例化 HTTP 客户端
         $http = new Client;
         // 发送 HTTP Get 请求
@@ -49,15 +55,9 @@ class Slug
     }
 
     // 获取百度翻译api;
-    private function getTranslateUrl($text)
+    private function getTranslateUrl($appid, $key, $text)
     {
-        $appid = $this->config['appId'];
-        $key = $this->config['appKey'];
         $salt = time();
-        // 如果没有配置百度翻译，自动使用兼容的拼音方案
-        if (empty($appid) || empty($key)) {
-            return $this->pinyin($text);
-        }
         $sign = md5($appid. $text . $salt . $key);
         // 构建请求参数
         $query = http_build_query([
